9e7b76d7aafcfa403ee92c1263ea43f31a42ad7d
Gerard Fajardo <gerard@zeta.haus>
2023-12-15 12:37:00 -0800

    Add socket listener to recieve challenge requests. Add handler for disconnected users

--- src/pages/Lobby/index.tsx
+++ src/pages/Lobby/index.tsx
@@ -6,44 +6,75 @@ const ENDPOINT = 'http://localhost:3000';
 
 const Lobby = () => {
     const [lobbyUsers, setLobbyUsers] = useState<Record<string, any>>({});
-    const [selectedUser, setSelectedUser] = useState<string>('');
+    const [challengedUserId, setChallengedUserId] = useState<string>('');
+    const [socket, setSocket] = useState<Socket | null>(null);
+    const [showDisconnectedMessage, setShowDisconnectedMessage] = useState<boolean>(false);
+    const [userReturned, setUserReturned] = useState<boolean>(false);
 
-    const currentUserId = localStorage.getItem('user_id');
+    const challengerUserId = localStorage.getItem('user_id');
 
     useEffect(() => {
         // Retrieve the token from local storage
-        const token = localStorage.getItem('accessToken'); // Replace 'tokenName' with your actual key
+        const token = localStorage.getItem('accessToken');
 
         // Initialize the socket only if the token exists
         if (token) {
-            const socket: Socket = io(ENDPOINT, {
+            const newSocket: Socket = io(ENDPOINT, {
                 auth: {
                     token: token,
                 },
             });
 
-            socket.on('lobbyUpdate', (users) => {
+            newSocket.on('lobbyUpdate', (users) => {
                 setLobbyUsers(users);
             });
 
-            socket.on('connect_error', (error) => {
+            newSocket.on('challenge-received', (data) => {
+                console.log('Challenge received:', data);
+            });
+
+            newSocket.on('connect_error', (error) => {
                 console.error('Connection error:', error);
             });
 
+            // Set the socket state
+            setSocket(newSocket);
+
             // Cleanup function to disconnect the socket when the component unmounts
             return () => {
-                socket.disconnect();
+                newSocket.disconnect();
             };
         }
     }, []);
 
+    // TODO - Add a useEffect hook to handle the user returning to the lobby
+    //       after being disconnected due to reloading the page or closing the tab
+
+    useEffect(() => {
+        if (challengedUserId && !lobbyUsers[challengedUserId]) {
+            setChallengedUserId('');
+            setTimeout(() => {
+                if (!userReturned) {
+                    setShowDisconnectedMessage(true);
+                }
+            }, 5000);
+            setTimeout(() => {
+
+                setShowDisconnectedMessage(false);
+
+            }, 10000);
+        }
+    }, [challengedUserId, lobbyUsers, userReturned]);
+
     const handleUserSelection = (userId: string) => {
-        setSelectedUser(userId);
+        setChallengedUserId(userId);
+        setUserReturned(false);
     };
 
     const handleChallenge = () => {
-        if (selectedUser) {
-            console.log(`Challenge request send to ${lobbyUsers[selectedUser].username}`);
+        if (challengedUserId && socket) {
+            socket.emit('challenge-request', { challengedUserId, challengerUserId });
+            console.log(`Challenge request sent to ${lobbyUsers[challengedUserId].username}`);
         }
     };
 
@@ -51,23 +82,26 @@ const Lobby = () => {
         <div className='lobby'>
             <h1>Lobby</h1>
             <ul>
-                {Object.values(lobbyUsers).filter(user => user.id !== currentUserId).map((user) => (
+                {Object.values(lobbyUsers).filter(user => user.id !== challengerUserId).map((user) => (
                     <li key={user.id}>
                         <input
                             type='radio'
                             id={user.id}
                             name='userSelection'
                             value={user.id}
-                            checked={selectedUser === user.id}
+                            checked={challengedUserId === user.id}
                             onChange={() => handleUserSelection(user.id)}
                         />
                         <label htmlFor={user.id}>{user.username}</label>
                     </li>
                 ))}
             </ul>
-            {selectedUser && (
+            {showDisconnectedMessage && (
+                <p>Selected user has been disconnected!</p>
+            )}
+            {challengedUserId && lobbyUsers[challengedUserId] && (
                 <div>
-                    <p>Challenge {lobbyUsers[selectedUser].username}?</p>
+                    <p>Challenge {lobbyUsers[challengedUserId].username}?</p>
                     <button onClick={handleChallenge}>Confirm</button>
                 </div>
             )}
