23c58bcd8b44d9846a80edba171c0cf9e11a1c61
Gerard Fajardo <gerard@zeta.haus>
2023-12-14 16:14:54 -0800

    Clean up and add some TODOs

--- src/pages/Homepage/index.tsx
+++ src/pages/Homepage/index.tsx
@@ -1,26 +1,11 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import Grid from '../../components/Grid';
 
 const Homepage: React.FC = () => {
-    const [data, setData] = useState(null);
-
-    useEffect(() => {
-      fetch('/api/data')
-          .then(response => response.json())
-          .then(data => setData(data))
-          .catch(error => console.error('Error fetching data:', error));
-  }, []); // Empty dependency array means this effect runs once on mount
-
-
     return (
         <>
             <h1>BATTLESHIP</h1>
             <Grid />
-
-            <h1>API Data</h1>
-            <div>
-                {data ? <div>{JSON.stringify(data)}</div> : <div>Loading...</div>}
-            </div>
         </>
     );
 }
