c260cdc4b0f5565fd99385b868e0ea1a8e3689ca
Gerard Fajardo <gerard@zeta.haus>
2023-12-18 22:46:31 -0800

    Implement ship placement hover effects before final placement

--- src/components/Grid/index.tsx
+++ src/components/Grid/index.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import './style.css';
 
+
 interface GridProps {
   setCurrentShip: (shipType: string) => void;
   currentShipSize: number;
@@ -13,55 +14,51 @@ const Grid: React.FC<GridProps> = ({ setCurrentShip, currentShipSize, ships, onS
   const cols = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
   const [startSquare, setStartSquare] = useState<string | null>(null);
   const [isFirstClick, setIsFirstClick] = useState(true);
+  const [hoverSquare, setHoverSquare] = useState<string | null>(null);
 
   const isOverlapping = (coordinates: string[]) => {
-      return coordinates.some(coordinate => 
-          Object.values(ships).flat().includes(coordinate)
-      );
+    return coordinates.some(coordinate => 
+        Object.values(ships).flat().includes(coordinate)
+    );
   };
 
   const isWithinBounds = (coordinates: string[]) => {
-      return coordinates.every(coordinate => {
-          const row = coordinate.charAt(0);
-          const col = parseInt(coordinate.substring(1));
-          return rows.includes(row) && col >= 1 && col <= cols.length;
-      });
+    return coordinates.every(coordinate => {
+        const row = coordinate.charAt(0);
+        const col = parseInt(coordinate.substring(1));
+        return rows.includes(row) && col >= 1 && col <= cols.length;
+    });
   };
 
-  const calculateShipCoordinates = (endSquare: string) => {
-      if (!startSquare || currentShipSize === 0) return;
+  const getCoordinates = (startSquare: string, endSquare: string) => {
+    if (!startSquare || currentShipSize === 0) return [];
 
-      const startRow = startSquare.charAt(0);
-      const startCol = parseInt(startSquare.substring(1));
-      const endRow = endSquare.charAt(0);
-      const endCol = parseInt(endSquare.substring(1));
+    const startRow = startSquare.charAt(0);
+    const startCol = parseInt(startSquare.substring(1));
+    const endRow = endSquare.charAt(0);
+    const endCol = parseInt(endSquare.substring(1));
 
-      let coordinates = [];
-      const isHorizontal = Math.abs(endCol - startCol) > Math.abs(rows.indexOf(endRow) - rows.indexOf(startRow));
-      const isIncrement = isHorizontal ? endCol > startCol : rows.indexOf(endRow) > rows.indexOf(startRow);
+    let coordinates = [];
+    const isHorizontal = Math.abs(endCol - startCol) > Math.abs(rows.indexOf(endRow) - rows.indexOf(startRow));
+    const isIncrement = isHorizontal ? endCol > startCol : rows.indexOf(endRow) > rows.indexOf(startRow);
 
-      if (isHorizontal) {
-          for (let i = 0; i < currentShipSize; i++) {
-              const col = isIncrement ? startCol + i : startCol - i;
-              if (col >= 1 && col <= cols.length) {
-                  coordinates.push(`${startRow}${col}`);
-              }
-          }
-      } else {
-          let startRowIndex = rows.indexOf(startRow);
-          for (let i = 0; i < currentShipSize; i++) {
-              const rowIndex = isIncrement ? startRowIndex + i : startRowIndex - i;
-              if (rowIndex >= 0 && rowIndex < rows.length) {
-                  coordinates.push(`${rows[rowIndex]}${startCol}`);
-              }
-          }
-      }
-
-      if (coordinates.length === currentShipSize && isWithinBounds(coordinates) && !isOverlapping(coordinates)) {
-          onShipPlacement(coordinates);
-      } else {
-          console.log("Invalid ship placement.");
-      }
+    if (isHorizontal) {
+        for (let i = 0; i < currentShipSize; i++) {
+            const col = isIncrement ? startCol + i : startCol - i;
+            if (col >= 1 && col <= cols.length) {
+                coordinates.push(`${startRow}${col}`);
+            }
+        }
+    } else {
+        let startRowIndex = rows.indexOf(startRow);
+        for (let i = 0; i < currentShipSize; i++) {
+            const rowIndex = isIncrement ? startRowIndex + i : startRowIndex - i;
+            if (rowIndex >= 0 && rowIndex < rows.length) {
+                coordinates.push(`${rows[rowIndex]}${startCol}`);
+            }
+        }
+    }
+    return coordinates;
   };
 
   const handleMouseDown = (square: string) => {
@@ -70,12 +67,21 @@ const Grid: React.FC<GridProps> = ({ setCurrentShip, currentShipSize, ships, onS
       setStartSquare(square);
       setIsFirstClick(false);
     } else {
-      calculateShipCoordinates(square);
+      const shipCoordinates = startSquare ? getCoordinates(startSquare, square) : [];
+      if (shipCoordinates.length === currentShipSize && isWithinBounds(shipCoordinates) && !isOverlapping(shipCoordinates)) {
+        onShipPlacement(shipCoordinates);
+      } else {
+        console.log("Invalid ship placement.");
+      }
       setIsFirstClick(true);
       setStartSquare(null);
       setCurrentShip('');
     }
   };
+
+  const projectionCoordinates = hoverSquare && startSquare 
+    ? getCoordinates(startSquare, hoverSquare) 
+    : [];
 
   return (
     <div className="grid-container">
@@ -90,12 +96,19 @@ const Grid: React.FC<GridProps> = ({ setCurrentShip, currentShipSize, ships, onS
           {cols.map((col) => {
             const square = `${row}${col}`;
             const isShipSquare = Object.values(ships).flat().includes(square);
-            const squareClass = isShipSquare ? "square ship-square" : "square";
+            const isProjectionSquare = projectionCoordinates.includes(square);
+            let squareClass = "square";
+            if (isShipSquare) {
+              squareClass += " ship-square";
+            } else if (isProjectionSquare) {
+              squareClass += " projection-square";
+            }
             return (
               <div
                 key={col}
                 className={squareClass}
                 onMouseDown={() => handleMouseDown(square)}
+                onMouseOver={() => setHoverSquare(square)}
               />
             );
           })}
