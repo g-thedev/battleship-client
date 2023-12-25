e5ab2d05016a6aafcc67d9fed45afc07ef36d66e
Gerard Fajardo <gerard@zeta.haus>
2023-12-24 17:05:56 -0800

    Add leave_pvp_lobby sokcet event

--- src/components/common/Nav/index.tsx
+++ src/components/common/Nav/index.tsx
@@ -2,13 +2,26 @@ import React, { useContext, useRef } from 'react';
 import { Link, useLocation, useNavigate } from 'react-router-dom';
 import { AuthContext } from '../../../context/AuthContext';
 import ConfirmationModal, { ConfirmationModalHandle } from '../Modal';
+import { useSocket } from '../../../context/SocketContext';
 import './style.css';
 
 const NavBar: React.FC = () => {
+    const { socket } = useSocket();
     const { isAuthenticated, logout } = useContext(AuthContext);
     const location = useLocation();
     const navigate = useNavigate();
     const modalRef = useRef<ConfirmationModalHandle>(null);
+
+    const handleMainMenuClick = () => {
+        if (location.pathname === '/lobby') {
+            const userId = localStorage.getItem('user_id');
+            if (userId && socket) {
+                // Emit the leave_pvp_lobby event when leaving the lobby
+                socket.emit('leave_pvp_lobby', { userId });
+            }
+        }
+        navigate('/');
+    };
 
     const handleLeaveGame = () => {
         modalRef.current?.show();
@@ -30,7 +43,7 @@ const NavBar: React.FC = () => {
                 {isAuthenticated ? (
                     <>
                         {location.pathname !== '/profile' && location.pathname !== '/lobby' && location.pathname !== '/game-setup' && location.pathname !== '/game-room' && <button onClick={() => navigate('/profile')}>Profile</button>}
-                        {location.pathname !== '/' && location.pathname !== '/game-setup' && location.pathname !== '/game-room' && <button onClick={() => navigate('/')}>Main Menu</button>}
+                        {location.pathname !== '/' && location.pathname !== '/game-setup' && location.pathname !== '/game-room' && <button onClick={handleMainMenuClick}>Main Menu</button>}
                         {location.pathname !== '/game-setup' && location.pathname !== '/game-room' && <button onClick={logout}>Logout</button>}
                         {(location.pathname === '/game-setup' || location.pathname === '/game-room') && <button onClick={handleLeaveGame}>Leave Game</button>}
                         <ConfirmationModal ref={ modalRef } />
